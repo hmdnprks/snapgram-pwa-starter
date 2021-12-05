e08c57a708bf6dd9ed50a2e90c2fbaee9fa0cca8
telkomdev-hamdan <hamdan.prakoso007@gmail.com>
2021-12-06 01:43:57 +0700

    Initialize PWA starter

---
+++ public/src/js/utility.js
@@ -0,0 +1,107 @@
+
+var FIREBASE_URL_POSTS = '';
+var FIREBASE_URL_SUBSCRIPTIONS = '';
+
+// INDEXED DB HELPER
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
+  }
+  if (!db.objectStoreNames.contains('sync-posts')) {
+    db.createObjectStore('sync-posts', {keyPath: 'id'});
+  }
+});
+
+function writeData(st, data) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.put(data);
+      return tx.complete;
+    });
+}
+
+function readAllData(st) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readonly');
+      var store = tx.objectStore(st);
+      return store.getAll();
+    });
+}
+
+function clearAllData(st) {
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.clear();
+      return tx.complete;
+    });
+}
+
+function deleteItemFromData(st, id) {
+  dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.delete(id);
+      return tx.complete;
+    })
+    .then(function() {
+      console.log('Item deleted!');
+    });
+}
+
+// COMMON HELPER
+
+function urlBase64ToUint8Array(base64String) {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+
+  for (var i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+function dataURItoBlob(dataURI) {
+  var byteString = atob(dataURI.split(',')[1]);
+
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  var arrayBuffer = new ArrayBuffer(byteString.length);
+  var _ia = new Uint8Array(arrayBuffer);
+  for (var i = 0; i < byteString.length; i++) {
+      _ia[i] = byteString.charCodeAt(i);
+  }
+
+  var dataView = new DataView(arrayBuffer);
+  var blob = new Blob([dataView], { type: mimeString });
+  return blob;
+}
+
+function convertObjectArray(objects){
+  var dataArray = [];
+  for(var key in objects){
+    dataArray.push(objects[key]);
+  }
+  return dataArray
+}
+
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) {
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length);
+  } else {
+    cachePath = string;
+  }
+  return array.indexOf(cachePath) > -1;
+}
